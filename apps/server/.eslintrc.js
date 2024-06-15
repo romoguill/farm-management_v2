90974a4ed366254ff367e25fc2ccde7fadf0ccf8
romoguill <romoguill@gmail.com>
2024-06-14 21:28:37 -0300

    Added tsup config to build trpc-api package and deleted old fastify project

--- apps/server/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ['@farm/eslint-config/server.js'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
