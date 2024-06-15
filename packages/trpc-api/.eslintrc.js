a93ea9e726b499192ed107d04b6b31eb3554a923
romoguill <romoguill@gmail.com>
2024-06-15 11:56:01 -0300

    Removed nextjs project using rc version for lts 14

--- packages/trpc-api/.eslintrc.js
+++ packages/trpc-api/.eslintrc.js
@@ -1,8 +1,8 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@farm/eslint-config/next.js'],
-  parser: '@typescript-eslint/parser',
+  extends: ["@farm/eslint-config/server.js"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
