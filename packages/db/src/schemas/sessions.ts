4ebb140e6c8e9b5f97ba504716bbf548546c40bd
Rodrigo Moguillansky <romoguill@gmail.com>
2024-06-17 09:19:12 -0300

    Set up basic user and session schema and a docker compose for local dev postgres instance

---
+++ packages/db/src/schemas/sessions.ts
@@ -0,0 +1,13 @@
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { users } from './users';
+
+export const sessions = pgTable('sessions', {
+  id: varchar('id', { length: 255 }).primaryKey(),
+  userId: uuid('id')
+    .notNull()
+    .references(() => users.id),
+  expiresAt: timestamp('expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+});
---
+++ packages/db/src/schemas/users.ts
@@ -0,0 +1,6 @@
+import { pgTable, serial, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey(),
+  email: varchar('email', { length: 100 }),
+});
