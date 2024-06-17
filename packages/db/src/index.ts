6fa25f8d5a38c5ed7b0eecb241673760685b4d61
Rodrigo Moguillansky <romoguill@gmail.com>
2024-06-17 08:57:25 -0300

    Created drizzle query client

---
+++ packages/db/src/index.ts
@@ -0,0 +1,13 @@
+import postgres from 'postgres';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { MigrationConfig } from 'drizzle-orm/migrator';
+
+const migrationClient = postgres(process.env.DATABASE_URL || '', { max: 1 });
+const dbMigrate = (config: string | MigrationConfig) =>
+  migrate(drizzle(migrationClient), config);
+
+const queryClient = postgres(process.env.DATABASE_URL || '');
+const db = drizzle(queryClient);
+
+export { db, dbMigrate };
