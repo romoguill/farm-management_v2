27ce881f556e5e9a3cac62eea39fdaf366106809
romoguill <romoguill@gmail.com>
2024-06-19 14:48:49 -0300

    Added google to user schema

---
+++ packages/db/drizzle/0001_faithful_nightshade.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "users" ADD COLUMN "username" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "google_id" numeric;
--- packages/db/src/schemas/users.ts
+++ packages/db/src/schemas/users.ts
@@ -1,6 +1,8 @@
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import { numeric, pgTable, text, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey(),
   email: varchar('email', { length: 100 }),
+  username: text('username'),
+  googleId: numeric('google_id'),
 });
