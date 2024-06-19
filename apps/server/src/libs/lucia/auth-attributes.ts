2406e495c122bece6d04a95d4714575eb3ddf48b
Rodrigo Moguillansky <romoguill@gmail.com>
2024-06-19 09:19:17 -0300

    Extended lucia adapter with session info

---
+++ apps/server/src/libs/lucia/auth-attributes.ts
@@ -0,0 +1,29 @@
+import { Lucia } from 'lucia';
+import { luciaAdapter } from '@farm/db';
+
+interface DatabaseUserAttributes {
+  google_id: number;
+  username: string;
+}
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+export const lucia = new Lucia(luciaAdapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      googleId: attributes.google_id,
+      username: attributes.username,
+    };
+  },
+});
