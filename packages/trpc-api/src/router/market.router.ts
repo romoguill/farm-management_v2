9b6f45e92da5489adf49e4c4bcb57a0f93f656a2
romoguill <romoguill@gmail.com>
2024-07-25 19:22:45 -0300

    Replaced placeholder response of market get data router with real implementation

--- packages/trpc-api/src/router/market.router.ts
+++ packages/trpc-api/src/router/market.router.ts
@@ -1,15 +1,35 @@
+import { TRPCError } from '@trpc/server';
 import { createTRPCRouter, publicProcedure } from '../trpc';
 import { parseMarketDataQuery } from '../utils/parse-market-query';
-import { marketQuerySchema } from '../validation-schemas';
+import { MarketData, marketQuerySchema } from '../validation-schemas';
 
 export const marketRouter = createTRPCRouter({
-  getData: publicProcedure.input(marketQuerySchema).query(({ input }) => {
+  getData: publicProcedure.input(marketQuerySchema).query(async ({ input }) => {
     const url = new URL(process.env.MARKET_API_BASE_URL!);
 
     const parsedQueryParams = new URLSearchParams(parseMarketDataQuery(input));
 
     url.search = parsedQueryParams.toString();
 
-    return { url };
+    try {
+      const response = await fetch(url);
+
+      const { data, error } = await response.json();
+
+      if (response.ok) {
+        return data;
+      } else {
+        throw new Error(`API failed with error: ${error.message}`);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new TRPCError({ message: error.message, code: 'BAD_REQUEST' });
+      }
+
+      throw new TRPCError({
+        message: 'Server Error',
+        code: 'INTERNAL_SERVER_ERROR',
+      });
+    }
   }),
 });
